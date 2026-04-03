1622e6dd5d85c3179b8d31b2859d9eb5be8a1ca2
Raunak Oberai <raunak.oberai@outlook.com>
2026-04-03 23:54:46 +1100

    Added env flag to model.py

--- src/model.py
+++ src/model.py
@@ -1,11 +1,15 @@
 # Just a placeholder for the model that will be trained. In a real application, this would contain the definition of the model architecture and any related functions.
 from sklearn.linear_model import LogisticRegression
+import os
+env = os.getenv("ENV", "dev")
 
 def run_model(sample=True):
+    
     print("Model simulation running...")
     return True
 
 if __name__ == "__main__":
+    print(f"Running in {env} environment")
     run_model()
 
 
